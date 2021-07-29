47e0b5ff0650b13156eedea3690f20efbf735b0e
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-07-29 15:37:41 -0500

    updated table info in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,13 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
+
+CREATE TABLE employee_info (
+    id INT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    department VARCHAR(50) NOT NULL,
+    salary INT NOT NULL,
+    manager VARCHAR(50)
+
+)
