311f806c07445e3166e000528421ca2bbd7ffe31
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-07-29 15:30:04 -0500

    updated README and added pseudocode to server js

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ The purpose of this project is to create data in a database via the command line
 Useful for viewing and editing data in a database environment. See "Video Walkthrough" to view how to create. Video upload still in progress.
 
 # Contributors
-Marie Valdovinos, Andrew Hardemon
+Marie Valdovinos, Andrew Hardemon (tutor)
 
 
 # Video Walkthrough
--- server.js
+++ server.js
@@ -13,19 +13,19 @@ const db = mysql.createConnection({
     },
     // console.log(`Connected to the classlist_db database.`)
 );
-
+//connect mysql and run askUser();
 db.connect((err) => {
-    if (err) throw err
-    askUser();
-})
-
+        if (err) throw err
+        askUser();
+    })
+    //run inquirer
 function askUser() {
     inquirer.prompt([{
         type: "list",
         name: "answer",
         message: "What would you like to do?",
         choices: ["view all roles", "view all employees", "view all departments", "add a department", "add a role", "add an employee", "update an employee role", "nothing else"]
-
+            //promise statement: send rsponse according to what the user does
     }]).then(response => {
         switch (response) {
             case a:
@@ -83,7 +83,7 @@ function viewRole() {
     var sql1 = "SELECT * FROM (role)"
     db.query(sql1, (err, rows) => {
         if (err) throw err;
-
+        //run console.table rows in the terminal and revert to run askUser() function for users next option
         console.table(rows)
         askUser();
     })
@@ -91,19 +91,21 @@ function viewRole() {
 
 function viewEmployees() {
     var sql2 = "SELECT * FROM (employees)"
+        //viewEmployees() = view complete list of every employee
     db.query(sql2, (err, rows) => {
         if (err) throw err;
-
+        //run console.table rows in the terminal and revert to run askUser() function for users next option
         console.table(rows)
         askUser();
     })
 }
 
 function viewDept() {
+    //viewDept: view all departments that exist
     var sql3 = "SELECT * FROM(department)"
     db.query(sql3, (err, rows) => {
         if (err) throw err;
-
+        //run console.table rows in the terminal and revert to run askUser() function for users next option
         console.table(rows)
         askUser();
 
@@ -111,40 +113,44 @@ function viewDept() {
 }
 
 function addDept() {
+    //addDept(): add a department
     var sql4 = "SELECT * FROM department INSERT INTO department (name) VALUES"
     db.query(sql4, (err, rows) => {
         if (err) throw err;
-
+        //run console.table rows in the terminal and revert to run askUser() function for users next option
         console.table(rows)
         askUser();
     })
 }
 
 function addRole() {
+    //addRole(): add a role for a certain employee
     var sql5 = "SELECT * FROM role INSERT INTO role (name) VALUES"
     db.query(sql5, (err, rows) => {
         if (err) throw err;
-
+        //run console.table rows in the terminal and revert to run askUser() function for users next option
         console.table(rows)
         askUser();
     })
 }
 
 function addEmployee() {
+    //add a new employee to table
     var sql6 = "SELECT * FROM employee INSERT INTO employee (name) VALUES"
     db.query(sql6, (err, rows) => {
         if (err) throw err;
-
+        //run console.table rows in the terminal and revert to run askUser() function for users next option
         console.table(rows)
         askUser();
     })
 }
 
 function updateEmployee() {
+    //update a current employees information
     var sql7 = "SELECT (employee id, lastname, firstname, salary, role, department, manager) FROM employees_db SET (name) VALUES WHERE id = NOT NULL"
     db.query(sql7, (err, rows) => {
         if (err) throw err;
-
+        //run console.table rows in the terminal and revert to run askUser() function for users next option
         console.table(rows)
         askUser();
     })
